111d34fa87029da39f21448b033b7c4cfde33736
Vaishnav Nugala <vaishnav.nugala@ecanarys.com>
2020-03-29 19:00:52 +0530

    Add project files.

---
+++ SampleApp2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SampleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, sum;
+            Console.WriteLine("Enter Value of a and b");
+            a = int.Parse(Console.ReadLine());
+            b = int.Parse(Console.ReadLine());
+            //OR
+            //a = Convert.ToInt32(Console.ReadLine());
+            //b = Convert.ToInt32(Console.ReadLine());
+            sum = a + b;
+
+            Console.WriteLine("The sum of {0} and {1} = {2}", a, b, sum);
+            Console.Read();
+
+        }
+    }
+}
